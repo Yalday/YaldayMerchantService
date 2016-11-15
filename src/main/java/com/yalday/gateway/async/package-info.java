b1c991dbdb0de7da289025d2621b531fc6e2457b
Phil Quinn <abetterway2feel@hotmail.co.uk>
2016-11-15 22:43:11 +0100

    jhipster created project

---
+++ src/main/java/com/yalday/gateway/async/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Async helpers.
+ */
+package com.yalday.gateway.async;
---
+++ src/main/java/com/yalday/gateway/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.yalday.gateway.web.rest.vm;
